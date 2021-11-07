12a3e8ae935034ff7d0af1856e03f78c8afc9ad6
Fabricio Rossi <fabri.r.nm@gmail.com>
2021-11-06 21:20:09 -0300

    New command & event handler

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ require('dotenv').config();
 const Client = require('./utils/structures/Client');
 const client = new Client();
 const { registerCommands, registerEvents } = require('./utils/Register');
-const unorm = require('unorm');
-const combining = /[\u0300-\u036F]/g;
 
 (async () => {
     client.config = new Map();
-    String.prototype.norm = function () {
-        return unorm.nfkd(this).replace(combining, '').toLowerCase();
-    }
+    client.commands = new Map();
+    client.aliases = new Map();
     await registerCommands(client, '../commands');
-    await registerEvents(client, '../events');
+    await registerEvents(client, "../events");
     client.login(process.env.BOT_TOKEN);
 })();
+
+const unorm = require('unorm');
+const combining = /[\u0300-\u036F]/g;
+
+String.prototype.norm = function () {
+    return unorm.nfkd(this).replace(combining, '').toLowerCase();
+};
--- src/utils/Register.js
+++ src/utils/Register.js
@@ -1,51 +1,40 @@
+const fs = require('fs');
 const path = require('path');
-const fs = require('fs').promises;
 const BaseCommand = require('./structures/BaseCommand');
 const BaseEvent = require('./structures/BaseEvent');
 
-async function registerCommands(client, dir = '') {
+async function registerCommands(client, dir) {
     const filePath = path.join(__dirname, dir);
-    const files = await fs.readdir(filePath);
-    for (const file of files) {
-        const stat = await fs.lstat(path.join(filePath, file));
-        if (stat.isDirectory()) {
-            await registerCommands(client, path.join(dir, file));
-        }
-        else {
-            if (file.endsWith('.js')) {
-                let cmdName = require(path.join(filePath, file));
-                if (cmdName.prototype instanceof BaseCommand) {
-                    try {
-                        const cmd = new cmdName();
-                        await client.commands.set(cmd.name, cmd);
-                        if (cmd.aliases) {
-                            await cmd.aliases.forEach(alias => client.aliases.set(alias, cmd));
-                        }
-                    }
-                    catch (err) {
-                        console.log(err);
-                    }
+    const commandFiles = fs.readdirSync(filePath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const Command = require(path.join(filePath, file));
+        if (Command.prototype instanceof BaseCommand) {
+            try {
+                const command = new Command();
+                client.commands.set(command.name, command);
+                if (command.aliases) {
+                    command.aliases.forEach(alias => client.aliases.set(alias, command));
                 }
+            }
+            catch (err) {
+                console.log(err);
             }
         }
     }
 }
 
-async function registerEvents(client, dir = '') {
-    const filePath = path.join(__dirname, dir)
-    const files = await fs.readdir(filePath);
-    for (const file of files) {
-        const stat = await fs.lstat(path.join(filePath, file));
-        if (stat.isDirectory()) {
-            registerEvents(client, path.join(dir, file));
-        }
-        else {
-            if (file.endsWith('.js')) {
-                const Event = require(path.join(filePath, file));
-                if (Event.prototype instanceof BaseEvent) {
-                    const event = new Event();
-                    client.on(event.name, event.run.bind(null, client));
-                }
+async function registerEvents(client, dir) {
+    const filePath = path.join(__dirname, dir);
+    const eventFiles = fs.readdirSync(filePath).filter(file => file.endsWith('.js'));
+    for (const file of eventFiles) {
+        const Event = require(path.join(filePath, file));
+        if (Event.prototype instanceof BaseEvent) {
+            try {
+                const event = new Event();
+                client.on(event.name, event.run.bind(null, client));
+            }
+            catch (err) {
+                console.log(err);
             }
         }
     }
